8de2333e3e3d7f997a62a9630ee36e2b7c91396a
itoooo <13117917+itoooo@users.noreply.github.com>
2018-07-22 11:05:08 +0900

    to work Ctrl-f in vi mode.

--- config.fish
+++ config.fish
@@ -1,7 +1,17 @@
 function fish_vi_cursor; end
+
 fish_vi_key_bindings
+
+# Enable Ctrl-F in vi insert mode
+function fish_user_key_bindings
+  for mode in insert default visual
+    fish_default_key_bindings -M $mode
+  end
+  fish_vi_key_bindings --no-erase
+end
 
 set -x GOPATH ~/go
 
 alias vi='/usr/bin/vim'
 
+set -g theme_color_scheme dracula
